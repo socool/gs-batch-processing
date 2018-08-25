48c774cdd8422cf16bd38e6bf56ea8bd36a1e36a
Kamolsit Mongkolsrisawat <kamolsit@computerlogy.com>
2018-08-25 21:30:40 +0700

    init project

---
+++ src/main/java/com/bcorpse/gsbatchprocessing/GsBatchProcessingApplication.java
@@ -0,0 +1,12 @@
+package com.bcorpse.gsbatchprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsBatchProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsBatchProcessingApplication.class, args);
+	}
+}
